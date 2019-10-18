e52f44c017878bf5118bacef9901c276e3c3fec2
César Gómez <55448868+CGGomez@users.noreply.github.com>
2019-10-18 19:02:47 +0200

    Add files via upload

--- UD2/Entrega1/Tarea9/Ej4.java
+++ UD2/Entrega1/Tarea9/Ej4.java
@@ -6,15 +6,21 @@ public class Ej4 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         String pal1;
+        int posicion;
 
-        System.out.println( "Introduzca una palabra" );
+        System.out.println( "Introduzca una frase" );
         pal1 = teclado.nextLine();
 
-        if (" " == (pal1.charAt(pal1.length()) / 2)){
-            System.out.println("En esta posición hay un espacio");
-        }
-        else{
-            System.out.println( "El caracter de esta posición es: "+ pal1.charAt(pal1.length()/2) );
+        posicion = pal1.length() / 2;
+
+        System.out.println(posicion);
+
+        for(int i =0; i <=pal1.length();i++){
+            if (i == posicion && pal1.charAt(i) == ' '){
+                System.out.println( "Aquí hay un espacio" );
+            }else if (i == posicion && pal1.charAt(i) != ' ' ) {
+                System.out.println( "La letra es: "+ pal1.charAt(posicion) );
+            }
         }
     }
 }
